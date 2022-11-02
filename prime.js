4f0de0b47a2ee159f11830334ff8b8f903a1f51c
Mahesh Thombare <maheshthombare@A-MacBook-Air.domain.name>
2022-11-02 19:23:31 +0530

    i have edit code by own

--- prime.js
+++ prime.js
@@ -1,11 +1,19 @@
 function prime(num){
-	let count;
+	let factor=0;
 	for(i=1;i<num;i++){
 		if(num%i){
+		factor++;
+		}
+	}
+	return factor;
+}
+
+
+
+let ans=prime(12)
+if(ans==2){
 			console.log("its prime number");
 		}
 		else{
 			console.log("its not prime number");
 		}
-	}
-}
